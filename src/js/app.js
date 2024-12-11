5a3ae27ae4049d78baf2b2a8e1912cd39c58e028
jsjm <153530524+jsjm1986@users.noreply.github.com>
2024-12-11 23:02:40 +0800

    Update app.js

--- src/js/app.js
+++ src/js/app.js
@@ -570,7 +570,7 @@ class VoiceAssistant {
                     'Authorization': key
                 },
                 body: JSON.stringify({
-                    model: 'glm-4',
+                    model: 'glm-4-plus',
                     messages: [
                         {
                             role: 'user',
